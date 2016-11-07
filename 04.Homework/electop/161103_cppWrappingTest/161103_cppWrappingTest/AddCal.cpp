e304aad6c8d9b6d1b27256fbdedda743fb0dbf22
Juhwan Yoo <electopx@gmail.com>
2016-11-07 10:11:08 +0900

    2016.11.07 Homework

---
+++ 04.Homework/electop/161103_cppWrappingTest/161103_cppWrappingTest/AddCal.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "AddCal.h"
+
+
+AddCal::AddCal(void)
+{
+	// nothing
+}
+
+
+AddCal::~AddCal()
+{
+	// nothing
+}
+
+
+int AddCal::Add(int _num1, int _num2)
+{
+	return (_num1 + _num2);
+}
