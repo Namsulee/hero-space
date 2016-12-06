8205a098f5d067406280dbf89d0e2cc57fbadebf
bongchae <tankengine@nate.com>
2016-12-06 23:33:33 +0900

    Voice Recognition

--- 02.RaspberryPI/01.Windows_10_IoT_Core/3.newRTGraph/newRTGraph/MainPage.xaml.cs
+++ 02.RaspberryPI/01.Windows_10_IoT_Core/3.newRTGraph/newRTGraph/MainPage.xaml.cs
@@ -4,9 +4,13 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
 using System;
-using System.Windows;
-using System.Collections.Generic;
 using Windows.Foundation;
+using System.Threading;
+
+using System.Collections;
+using System.Collections.ObjectModel;
+
+
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -18,12 +22,28 @@ namespace newRTGraph
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Timer periodicTimer;
+        Line vline = new Line();
+        double xstep = 0;
+        double xtmp = 0;
+        double ytmp = 0;
+
+        private Random random = new Random();
+        //private int pointIndex = 0;
+        //private int pointIndex2 = 0;
+        private long count = 0;
+        int graph_line_x = 0;
+        int graph_line_y = 0;
+        ObservableCollection<Point> Obdatapoints = new ObservableCollection<Point>();
+
+
         public MainPage()
         {
             this.InitializeComponent();
             //drawExample();
             //drawLine();
             DrawChart();
+  
         }
         private void drawExample()
         {
@@ -54,6 +74,7 @@ namespace newRTGraph
 
             this.Content = myStackPanel;
         }
+
         private void drawLine()
         {
             // Create a StackPanel to contain the shape.
@@ -81,7 +102,6 @@ namespace newRTGraph
 
         private void DrawChart()
         {
-
             canGraph.Children.Clear();
             double[] RectWall;
             RectWall = new double[4];
@@ -194,304 +214,10 @@ namespace newRTGraph
             yaxis_path.Data = yxaxis_geom;
             canGraph.Children.Add(yaxis_path);
         }
-        /*
-        private void DrawChart(Canvas canGraph, List<NameValueItem>[] pList, double axisx_min, double axisx_max, double data_min, double data_max)
+  
+  
 
-        {
 
-            canGraph.Children.Clear();
 
-
-
-            double[] RectWall;
-
-            RectWall = new double[4];
-
-            RectWall[0] = 0;                // left 
-
-            RectWall[1] = 0;                // top
-
-            RectWall[2] = canGraph.ActualWidth;   // right
-
-            RectWall[3] = canGraph.ActualHeight;  // bottom
-
-
-
-            double[] RectGap;
-
-            RectGap = new double[4];
-
-            RectGap[0] = 30;    // left 
-
-            RectGap[1] = 20;    // top
-
-            RectGap[2] = 20;    // right
-
-            RectGap[3] = 20;    // bottom
-
-
-
-            double[] RectChart;
-
-            RectChart = new double[4];
-
-            RectChart[0] = RectWall[0] + RectGap[0]; // left 
-
-            RectChart[1] = RectWall[1] + RectGap[1]; // top
-
-            RectChart[2] = RectWall[2] - RectGap[2]; // right
-
-            RectChart[3] = RectWall[3] - RectGap[3]; // bottom
-
-
-
-            double fChartWidth = RectChart[2] - RectChart[0];
-
-            double fChartHeight = RectChart[3] - RectChart[1];
-
-
-
-            int xGridNum = 5;
-
-            int yGridNum = 10;
-
-            double fGridGapX = fChartWidth / xGridNum;
-
-            double fGridGapY = fChartHeight / yGridNum;
-
-
-
-            // 차트 그리기.
-
-            Rectangle pChartRect = new Rectangle();
-
-            pChartRect.StrokeThickness = 1;
-
-            pChartRect.Fill = new SolidColorBrush(Colors.Black);
-
-            pChartRect.Stroke = new SolidColorBrush(Colors.Yellow);
-
-            pChartRect.Width = fChartWidth;
-
-            pChartRect.Height = fChartHeight;
-
-            Canvas.SetLeft(pChartRect, RectChart[0]);
-
-            Canvas.SetTop(pChartRect, RectChart[1]);
-
-            canGraph.Children.Add(pChartRect);
-
-
-
-            // x축 그리기.
-
-            int iCount = 0;
-
-            GeometryGroup xaxis_geom = new GeometryGroup();
-
-            for (double x = 0; x <= fChartWidth; x += fGridGapX)
-
-            {
-
-                LineGeometry xtick = new LineGeometry();
-
-                xtick.StartPoint = new Point(x + RectChart[0], RectChart[3] + 5);
-
-                xtick.EndPoint = new Point(x + RectChart[0], RectChart[1]);
-
-                xaxis_geom.Children.Add(xtick);
-
-
-
-                TextBlock xlabel = new TextBlock();
-
-                int ivalue = (int)axisx_min + (int)(axisx_max - axisx_min) / xGridNum * iCount;
-
-                xlabel.Text = ivalue.ToString();
-
-                xlabel.FontSize = 10;
-
-                Canvas.SetLeft(xlabel, x + RectChart[0]);
-
-                Canvas.SetTop(xlabel, RectChart[3] + 5);
-
-                canGraph.Children.Add(xlabel);
-
-                iCount = iCount + 1;
-
-            }
-
-
-
-            Path xaxis_path = new Path();
-
-            xaxis_path.StrokeThickness = 1;
-
-            xaxis_path.Stroke = new SolidColorBrush(Colors.Green);
-
-            xaxis_path.Data = xaxis_geom;
-
-            canGraph.Children.Add(xaxis_path);
-
-
-
-            // y축 그리기.
-
-            iCount = 0;
-
-            GeometryGroup yxaxis_geom = new GeometryGroup();
-
-            for (double y = 0; y <= fChartHeight; y += fGridGapY)
-
-            {
-
-                LineGeometry ytick = new LineGeometry();
-
-                ytick.StartPoint = new Point(RectChart[0] - 5, RectChart[3] - y);
-
-                ytick.EndPoint = new Point(RectChart[2], RectChart[3] - y);
-
-                yxaxis_geom.Children.Add(ytick);
-
-
-
-                TextBlock ylabel = new TextBlock();
-
-                int ivalue = (int)data_min + (int)(data_max - data_min) / yGridNum * iCount;
-
-                ylabel.Text = ivalue.ToString();
-
-                ylabel.FontSize = 10;
-
-                Canvas.SetLeft(ylabel, RectChart[0] - 20);
-
-                Canvas.SetTop(ylabel, RectChart[3] - y - ylabel.FontSize);
-
-                canGraph.Children.Add(ylabel);
-
-                iCount = iCount + 1;
-
-            }
-
-
-
-            Path yaxis_path = new Path();
-
-            yaxis_path.StrokeThickness = 1;
-
-            yaxis_path.Stroke = new SolidColorBrush(Colors.Green);
-
-            yaxis_path.Data = yxaxis_geom;
-
-            canGraph.Children.Add(yaxis_path);
-
-
-
-            // data 그리기.
-
-            double x1 = 0;
-
-            double y1 = 0;
-
-            double x2 = 0;
-
-            double y2 = 0;
-
-
-
-            int idim = 0;
-
-            idim = pList.Length;
-
-            if (idim > 0)
-
-            {
-
-                for (int i = 0; i < idim; i++)
-
-                {
-
-                    GeometryGroup data_geom = new GeometryGroup();
-
-
-
-                    List<NameValueItem> newitems = pList[i];
-
-                    double xstep = fChartWidth / newitems.Count;
-
-
-
-                    for (int j = 0; j < newitems.Count - 1; j++)
-
-                    {
-
-                        LineGeometry vline = new LineGeometry();
-
-
-
-                        x1 = RectChart[0] + xstep * j;
-
-                        y1 = RectChart[3] - fChartHeight * ((newitems[j].Value - data_min) / (data_max - data_min));
-
-                        if (y1 < RectChart[1])
-
-                            y1 = RectChart[1];
-
-                        if (y1 > RectChart[3])
-
-                            y1 = RectChart[3];
-
-                        x2 = RectChart[0] + xstep * (j + 1);
-
-                        y2 = RectChart[3] - fChartHeight * ((newitems[j + 1].Value - data_min) / (data_max - data_min));
-
-                        if (y2 < RectChart[1])
-
-                            y2 = RectChart[1];
-
-                        if (y2 > RectChart[3])
-
-                            y2 = RectChart[3];
-
-                        vline.StartPoint = new Point(x1, y1);
-
-                        vline.EndPoint = new Point(x2, y2);
-
-                        data_geom.Children.Add(vline);
-
-                    }
-
-
-
-                    Path value_path = new Path();
-
-                    value_path.StrokeThickness = 1;
-
-                    if (i == 0)
-
-                        value_path.Stroke = new SolidColorBrush(Colors.Red);
-
-                    else if (i == 1)
-
-                        value_path.Stroke = new SolidColorBrush(Colors.Green);
-
-                    else if (i == 2)
-
-                        value_path.Stroke = new SolidColorBrush(Colors.Blue);
-
-                    else
-
-                        value_path.Stroke = new SolidColorBrush(Colors.Yellow);
-
-                    value_path.Data = data_geom;
-
-                    canGraph.Children.Add(value_path);
-
-                }
-
-            }
-            
-        }
-    */
     }
 }
---
+++ 02.RaspberryPI/01.Windows_10_IoT_Core/4.VoiceRecognition_test/vr_test/MainPage.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.Media.SpeechRecognition;
+using Windows.Media.SpeechSynthesis;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace vr_test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        // Grammer File
+        private const string SRGS_FILE = "input11.xml";
+        // Speech Recognizer
+        private SpeechRecognizer recognizer;
+        private SpeechSynthesizer synth;
+        private MediaElement myMedia;
+        private SpeechSynthesisStream sStream;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Unloaded += MainPage_Unloaded;
+
+            initializeSpeechRecognizer();
+            initSynthsizer();
+
+        }
+
+
+        private async void MainPage_Unloaded(object sender, object args)
+        {
+            // Stop recognizing
+            await recognizer.ContinuousRecognitionSession.StopAsync();
+
+            // Release pins
+            recognizer.Dispose();
+            synth.Dispose();
+
+            recognizer = null;
+            synth = null;
+        }
+
+        private async void initializeSpeechRecognizer()
+        {
+            try
+            {
+                // Initialize recognizer
+
+                recognizer = new SpeechRecognizer();
+
+                recognizer.StateChanged += RecognizerStateChanged;
+                recognizer.ContinuousRecognitionSession.ResultGenerated += RecognizerResultGenerated;
+
+                Debug.WriteLine("Result: 1. Event Handler Created.");
+                
+                // Load Grammer file constraint
+                string fileName = String.Format(SRGS_FILE);
+                StorageFile grammarContentFile = await Package.Current.InstalledLocation.GetFileAsync(fileName);
+                Debug.WriteLine("Result: 2. Storage file Created "+ grammarContentFile.ToString());
+
+                SpeechRecognitionGrammarFileConstraint grammarConstraint = new SpeechRecognitionGrammarFileConstraint(grammarContentFile);
+                Debug.WriteLine("Result: 3. Grammar File Contraint " + grammarConstraint.ToString());
+
+                // Add to grammer constraint
+                recognizer.Constraints.Add(grammarConstraint);
+
+                // Compile grammer
+                SpeechRecognitionCompilationResult compilationResult = await recognizer.CompileConstraintsAsync();
+
+                Debug.WriteLine("Status: 4. compilation Status " + compilationResult.Status.ToString());
+                // If successful, display the recognition result.
+                if (compilationResult.Status == SpeechRecognitionResultStatus.Success)
+                {
+                    await recognizer.ContinuousRecognitionSession.StartAsync();
+                    Debug.WriteLine("Result: 5. compilation Result Success " + compilationResult.ToString());
+
+                   
+                }
+                else
+                {
+                    Debug.WriteLine("Status: 6. Compilation Result not success. " + compilationResult.Status);
+                }
+
+                // Send the stream to the media object.
+                //mediaElement.SetSource(stream, stream.ContentType);
+                //mediaElement.Play();
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Recognition Error: " + e.ToString());
+            }
+
+        }
+        // Recognizer generated results
+
+        private async void initSynthsizer()
+        {
+            try
+            {
+                synth = new SpeechSynthesizer();
+
+                // Generate the audio stream from plain text.
+                sStream = await synth.SynthesizeTextToStreamAsync("");
+
+                var mediaElement = new MediaElement();
+                mediaElement.SetSource(sStream, sStream.ContentType);
+                mediaElement.Play();
+
+                sStream.Seek(0);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("SpeechSynthesizer Error: " + e.ToString());
+            }
+        }
+        private void RecognizerResultGenerated(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            // Output debug strings
+            Debug.WriteLine("Recognition:  " + args.Result.Status);
+            Debug.WriteLine("Recognition:  " + args.Result.Text);
+
+            int count = args.Result.SemanticInterpretation.Properties.Count;
+
+            Debug.WriteLine("Count: " + count);
+            Debug.WriteLine("Tag: " + args.Result.Constraint.Tag);
+                   
+
+    }
+
+        // Recognizer state changed
+        private void RecognizerStateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
+        {
+            Debug.WriteLine("Speech recognizer state: " + args.State.ToString());
+        }
+
+        private async void tts(String a)
+        {
+            try
+            {
+                // Generate the audio stream from plain text.
+                sStream = await synth.SynthesizeTextToStreamAsync(a);
+
+                var mediaElement = new MediaElement();
+                mediaElement.SetSource(sStream, sStream.ContentType);
+                mediaElement.Play();
+
+
+                sStream.Seek(0);
+
+
+
+                // Send the stream to the media object.
+                //mediaElement.SetSource(stream, stream.ContentType);
+                //mediaElement.Play();
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Error: " + e.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            tts("Click first Button and waiting for your command");
+        }
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            tts("Test for foreign language");
+        }
+
+        private void Button_Click_2(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            tts("click third button");
+        }
+    }
+}
